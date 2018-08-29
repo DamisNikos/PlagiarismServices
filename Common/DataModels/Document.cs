3b9f633a3e621d327542929062ca354e2c6ded11
nikos damis <nikosdamis@hotmail.com>
2018-08-29 14:02:49 +0300

    test02

--- Common/DataModels/Document.cs
+++ Common/DataModels/Document.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-//TEST??//
+//TEST2??//
 namespace Common.DataModels
 {
     public class Document
