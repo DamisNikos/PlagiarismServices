8035d00a317e4798edce4af6f0b0d1127454cd38
nikos damis <nikosdamis@hotmail.com>
2018-09-05 14:20:52 +0300

    palio alla douleuei

--- Common/ResultsModel/ResultsContext.cs
+++ Common/ResultsModel/ResultsContext.cs
@@ -8,7 +8,7 @@ namespace Common.ResultsModel
         public DbSet<CommonPassage> Passages { get; set; }
 
         public ResultsContext()
-        : base("Server=172.26.190.254;Initial Catalog=ResultsDatabase;Persist Security Info=False;User ID=sa;Password=Ceid@5202;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=3000;")
+        : base("Server=62.1.231.194;Initial Catalog=Documents;Persist Security Info=False;User ID=sa;Password=Ceid@5202;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=3000;")
         {
             System.Data.Entity.Database.SetInitializer(new CreateDatabaseIfNotExists<ResultsContext>());
         }
